106b8bfd5297f0d27fd834265c183749d00f164c
developer <developer@MacBook-Pro-developer.local>
2023-01-30 17:05:01 +0200

    edits added in task 5 with autocomplete

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,15 @@
 const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
+console.log("value -> ", typeof textInput.value);
+
+if (textInput.value === "") {
+  textOutput.textContent = "Anonymous";
+} else {
+  textOutput.textContent = textInput.value;
+}
 
 textInput.addEventListener("input", (event) => {
   textOutput.textContent = event.target.value;
-
   if (event.target.value === "") {
     console.log(textOutput);
     textOutput.textContent = "Anonymous";
